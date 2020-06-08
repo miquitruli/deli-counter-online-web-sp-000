e2301e49db787614def64ca2d9cf66cdd44b6cc4
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-08 20:22:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+
+
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size<1
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number("The line is currently empty.")
