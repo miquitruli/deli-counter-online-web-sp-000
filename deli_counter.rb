cb3544206046bcd22a8c970be18f93e76d7871e3
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-08 20:43:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,10 @@ katz_deli = []
 def line(katz_deli)
   line = []
   if katz_deli.size>1
-    katz_deli.each_with_index do|name, index|
-      line << "#{index+1}. #{name}"
-    puts "The line is currently: #{line.join(" ")}"
-  end
-  elsif katz_deli<1
+    katz_deli.each_with_index {|name, index|
+      line << "#{index+1}. #{name}"}
+      puts "The line is currently: #{line.join(" ")}"
+  elsif katz_deli.size<1
     puts "The line is currently empty."
   end
 end
